8051b14e06b2f6b7fbe306cadd38d5a678710a43
Ferdy Fauzi <payen83@gmail.com>
2025-11-21 10:20:44 +0800

    update get reports

--- lib/app/controllers/report.controller.dart
+++ lib/app/controllers/report.controller.dart
@@ -1,14 +1,13 @@
 import 'dart:developer';
 import 'package:get/get.dart';
-import 'package:dio/dio.dart';
 import 'package:myflutter_nov2025/app/services/api.service.dart';
 import '../models/report.model.dart';
 
 class ReportController extends GetxController {
   var reportList = <Report>[
-    // Report(id: 1, title: 'Fire Inspection', date: '10-11-2025', category: 'safety', imagePath: ''),
-    // Report(id: 2, title: 'Broken Window', date: '08-11-2025', category: 'property', imagePath: ''),
-    // Report(id: 3, title: 'Unauthorized Access Server Room', date: '05-11-2025', category: 'security', imagePath: ''),
+    //Report(id: 1, title: 'Fire Inspection', date: '10-11-2025', category: 'safety', imagePath: ''),
+    //Report(id: 2, title: 'Broken Window', date: '08-11-2025', category: 'property', imagePath: ''),
+    //Report(id: 3, title: 'Unauthorized Access Server Room', date: '05-11-2025', category: 'security', imagePath: ''),
   ].obs;
 
   @override
@@ -20,32 +19,28 @@ class ReportController extends GetxController {
   getReports() async {
     log('Starting getReports()');
     try {
-      var result = await api.getDio('/products');
+      var result = await api.getDio('/reports');
       log('API call successful, result: $result');
       if (result != null && result.statusCode == 200) {
         log('Status code is 200, processing data...');
         reportList.assignAll(
-          (result.data['products'] as List).map((report) {
+          (result.data['data'] as List).map((report) {
             return Report(
               id: report['id'],
               title: report['title'],
-              date: report['price'].toString(),
+              date: report['date'],
               category: report['category'],
-              imagePath: report['thumbnail'] != null
-                  ? report['thumbnail'].toString()
+              imagePath: report['image_path'] != null
+                  ? '${api.baseurl}/${report['image_path']}'
                   : '',
             );
           }).toList(),
         );
-        log('Report list updated with ${reportList.length} items');
       } else {
         log('Unexpected status code: ${result?.statusCode}');
       }
     } catch (e) {
       log('Error in getReports: ${e.toString()}');
-      if (e is DioException) {
-        log('Dio Error: ${e.type} - ${e.message}');
-      }
     }
   }
 }
--- lib/app/services/api.service.dart
+++ lib/app/services/api.service.dart
@@ -4,29 +4,20 @@ import 'package:dio/dio.dart';
 final ApiServices api = ApiServices();
 
 class ApiServices {
-  var baseurl = 'https://dummyjson.com';
-
-  // Future getHttp(String path, {Map<String, dynamic>? params}) async {
-  //   String url = baseurl + path;
-  //   log(url);
-  //   final response = await http.get(Uri.parse(url));
-  //   log(response.statusCode.toString());
-  // }
+  var baseurl = 'http://10.0.2.2:3000/api';
 
   Future getDio(String path, {Map<String, dynamic>? params}) async {
     String url = baseurl + path;
+    log(url);
     var headers = {'accept': 'application/json'};
-
-    try {
-      // log('Making GET request to: $l');
-      var response = await Dio().get(
-        url, 
-        queryParameters: params,
-        options: Options(headers: headers)
-      );
-      return response;
-    } catch (e) {
-      log('API Error: ${e.toString()}');
-    }
+    var response = await Dio().get(
+      url,
+      queryParameters: params,
+      options: Options(headers: headers),
+    );
+    return response;
+    // } catch (e) {
+    //   log('API Error: ${e.toString()}');
+    // }
   }
 }
